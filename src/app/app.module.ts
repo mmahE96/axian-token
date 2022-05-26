95f816bab8a88119f98790de78b6f5bf606ac607
Mahir Mulaomerovic <mahir.cs@hotmail.com>
2022-05-26 12:07:44 +0200

    MVP design done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+import { FaucetComponent } from './faucet/faucet.component';
+import { AccountInfoComponent } from './account-info/account-info.component';
+import { TransferTokensComponent } from './transfer-tokens/transfer-tokens.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    FaucetComponent,
+    AccountInfoComponent,
+    TransferTokensComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/transfer-tokens/transfer-tokens.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transfer-tokens',
+  templateUrl: './transfer-tokens.component.html',
+  styleUrls: ['./transfer-tokens.component.css']
+})
+export class TransferTokensComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
